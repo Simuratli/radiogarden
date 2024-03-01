3b787bf645b9a2dd80b0733b9bca86d9c1066bf4
Simuratli Eljan <simuratli@gmail.com>
2024-03-01 12:28:45 +0400

    t

--- src/components/Audio/Audio.tsx
+++ src/components/Audio/Audio.tsx
@@ -1,22 +1,27 @@
-import React, { ChangeEvent, useRef, useState, useEffect } from "react";
+import React, { ChangeEvent, useRef, useState,useEffect } from "react";
 import "../../styles/components/audio.scss";
 import { PlayButton, NextPrev, Volume } from "../../components";
-import { AudioPropTypes } from "./Audio.types";
-function Audio({ station }: AudioPropTypes) {
+import {AudioPropTypes} from './Audio.types'
+function Audio({station}:AudioPropTypes) {
+
+
   useEffect(() => {
-    if (station) {
-      setIsPlaying(false);
-      setUpdate(true);
+    if(station){
+      setIsPlaying(false)
+      setUpdate(true)
       setTimeout(() => {
-        setUpdate(false);
+        setUpdate(false)
       }, 100);
+    }else{
+      setUpdate(true)
     }
-  }, [station]);
+  }, [station])
+  
 
   const audioRef = useRef<any>(null);
   const [isPlaying, setIsPlaying] = useState(true);
   const [volume, setVolume] = useState(100);
-  const [update, setUpdate] = useState(false);
+  const [update, setUpdate] = useState(false)
 
   const handlePlay = () => {
     if (audioRef.current) {
@@ -53,17 +58,16 @@ function Audio({ station }: AudioPropTypes) {
           />
           <NextPrev next />
         </div>
-        <Volume
-          value={volume}
-          min={0}
-          max={100}
-          onChange={handleChangeVolume}
-        />
-        {!update && (
-          <audio ref={audioRef} autoPlay>
-            <source src={station ? station.url : ""} type="audio/mpeg" />
-          </audio>
-        )}
+        <Volume value={volume} min={0} max={100} onChange={handleChangeVolume} />
+        {
+          !update && <audio ref={audioRef} autoPlay>
+          <source
+            src={station ? station.url : "https://radio.garden/api/ara/content/listen/VBZNZCLB/channel.mp3?r=1&1708426717009"}
+            type="audio/mpeg"
+          />
+        </audio>
+        }
+        
       </div>
     </div>
   );
